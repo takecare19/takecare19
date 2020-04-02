dffe021fd86be091bf4eef57b3587c10867f6e04
Susie Kim <susie.kim@rewardops.com>
2020-04-02 13:36:44 -0400

    Install vuetify

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
+    <v-app-bar flat>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Take Care 19</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu left bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn class="mx-2" fab dark v-on="on">
+            <v-icon dark>mdi-plus</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
     <h1>Welcome to takecare 19!</h1>
     <ResourceList :resources="this.resources" />
   </div>
