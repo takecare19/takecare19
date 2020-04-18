f7a6e3ee7c030ffe249b8772a4c5cba8f8d503c6
Susie Kim <susie.kim@rewardops.com>
2020-04-17 20:54:58 -0400

    Add link to google form

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,7 +10,13 @@
     <v-btn to="/about" class="about--desktop" text>
       about
     </v-btn>
-    <v-btn to="/submit" class="submit--desktop" color="primary" depressed>
+    <v-btn
+      href="https://docs.google.com/forms/d/e/1FAIpQLSepscN5ZHSy7hQ2rxS40Z93ItSWljyvHAsN8gBy6k1HngdV4Q/viewform"
+      target="_blank"
+      class="submit--desktop"
+      color="primary"
+      depressed
+    >
       add resource
     </v-btn>
     <router-link to="/submit" class="submit--mobile">
