b5a34922a50ee78574104b9b8f22d0bfc795a2c6
Susie Kim <susie.kim@rewardops.com>
2020-04-26 20:36:09 -0400

    add loading resource and categories states

---
+++ src/components/CategoriesListLoader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="categories-loader">
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+    <v-skeleton-loader class="ma-1" type="chip"></v-skeleton-loader>
+  </div>
+</template>
+
+<style lang="scss">
+.categories-loader {
+  display: flex;
+  flex-wrap: wrap;
+
+  .v-skeleton-loader:nth-child(3n) .v-skeleton-loader__chip.v-skeleton-loader__bone {
+    width: 150px !important;
+  }
+}
+</style>
---
+++ src/components/ResourceCardLoader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-card class="mt-5" style="border: 0.5px solid lightgray;">
+      <v-skeleton-loader type="article, divider, list-item-two-line"></v-skeleton-loader>
+    </v-card>
+    <v-card class="mt-5" style="border: 0.5px solid lightgray;">
+      <v-skeleton-loader type="article, divider, list-item-two-line"></v-skeleton-loader>
+    </v-card>
+    <v-card class="mt-5" style="border: 0.5px solid lightgray;">
+      <v-skeleton-loader type="article, divider, list-item-two-line"></v-skeleton-loader>
+    </v-card>
+  </div>
+</template>
